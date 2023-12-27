1f40f95627e25209987f129d45ca3eca5bf06730
Benny Daon <benny@tuzig.com>
2023-12-27 16:29:01 +0200

    Using lua copilot

--- .config/nvim/lua/daonb/plugins.lua
+++ .config/nvim/lua/daonb/plugins.lua
@@ -9,7 +9,7 @@ return require('packer').startup(function(use)
   use 'nvim-lua/plenary.nvim'
   use 'rose-pine/vim'
   use {
-	    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	    'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	    -- or                            , branch = '0.1.x',
 	    --   requires = { {'nvim-lua/plenary.nvim'} }
   }
@@ -49,5 +49,8 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'},
 	  }
   }
-  use("github/copilot.vim")
+  use { "zbirenbaum/copilot.lua" ,
+      cmd = "Copilot",
+      event = "InsertEnter",
+  }
 end)
